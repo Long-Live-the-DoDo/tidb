a69c1eae0319afe7ca716736e66c67eda61bcbb4
disksing <i@disksing.com>
2022-01-08 20:57:18 +0800

    update default save point config
    Signed-off-by: disksing <i@disksing.com>

--- store/gcworker/gc_worker.go
+++ store/gcworker/gc_worker.go
@@ -169,7 +169,7 @@ const (
 	gcWorkerServiceSafePointID = "gc_worker"
 
 	gcSavePointIntervalKey     = "tikv_gc_save_point_interval"
-	gcDefaultSavePointInterval = time.Minute * 20
+	gcDefaultSavePointInterval = time.Hour
 	gcSavePointLifeTimeKey     = "tikv_gc_save_point_life_time"
 	gcDefaultSavePointLifeTime = time.Hour * 24
 
